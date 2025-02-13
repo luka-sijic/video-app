625535ec04738264540a4d8ebcb5fb3c45cefefb
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2025-02-13 14:44:57 -0700

    Update server.go

--- backend/server/server.go
+++ backend/server/server.go
@@ -8,7 +8,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-    	"github.com/labstack/echo/v4/middleware"
+    "github.com/labstack/echo/v4/middleware"
 )
 
 var jwtToken string

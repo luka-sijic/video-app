7e51dd3f21dd568efc98dc95e3f5607f49b97a6b
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2025-02-13 14:35:27 -0700

    Update database.go

--- backend/database/database.go
+++ backend/database/database.go
@@ -3,26 +3,61 @@ package database
 import (
 	"fmt"
 	"context"
-//	"github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/redis/go-redis/v9"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+    	"github.com/aws/aws-sdk-go-v2/credentials"
+    	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"log"
 	"os"
 )
 
 var DB *pgxpool.Pool
 var RDB *redis.Client
+var R2 *s3.Client
 
-//func init() {
-//    err := godotenv.Load(".env")
-//    if err != nil {
-//        log.Fatalf("Error loading .env file: %v", err)
-//    }
-//}
+func init() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+}
 
 func Connect() {
+	err := InitR2Client(os.Getenv("ACCOUNT_ID"), os.Getenv("ACCESSKEYID"), os.Getenv("ACCESSKEYSECRET"))
+	if err != nil {
+		log.Fatalf("Failed to initialize R2 client: %v", err)
+	}
 	ConnectPostgres()
 	ConnectRedis()
+}
+
+func InitR2Client(accountId, accessKeyId, accessKeySecret string) error {
+    r2Resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+        return aws.Endpoint{
+            URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+        }, nil
+    })
+
+    cfg, err := config.LoadDefaultConfig(context.TODO(),
+        config.WithEndpointResolverWithOptions(r2Resolver),
+        config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+            accessKeyId,
+            accessKeySecret,
+            "",
+        )),
+        config.WithRegion("auto"),
+    )
+    if err != nil {
+        return fmt.Errorf("failed to load R2 configuration: %w", err)
+    }
+
+    // Initialize the global R2 client
+    R2 = s3.NewFromConfig(cfg)
+    fmt.Println("R2 connected")
+    return nil
 }
 
 func ConnectRedis() {
